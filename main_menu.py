53ecd200b3a31b072f7d65679b812c574dbee8ef
Lukasz Wieckowski <lukasz_wieckowski@zut.edu.pl>
2020-11-09 20:47:54 +0100

    Create main_menu.py

---
+++ main_menu.py
@@ -0,0 +1,224 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\lukasz\Desktop\main.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(630, 497)
+        Dialog.setAutoFillBackground(False)
+        Dialog.setStyleSheet("background-color: rgb(0, 0, 0);")
+        self.pushButton_1 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_1.setGeometry(QtCore.QRect(20, 140, 191, 51))
+        self.pushButton_1.setStyleSheet("background-color: rgb(0, 85, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_1.setObjectName("pushButton_1")
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(20, 10, 211, 71))
+        font = QtGui.QFont()
+        font.setFamily("MV Boli")
+        font.setPointSize(30)
+        self.label.setFont(font)
+        self.label.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(260, 30, 341, 31))
+        font = QtGui.QFont()
+        font.setFamily("MV Boli")
+        font.setPointSize(14)
+        self.label_2.setFont(font)
+        self.label_2.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label_2.setObjectName("label_2")
+        self.pushButton_2 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_2.setGeometry(QtCore.QRect(20, 200, 191, 51))
+        self.pushButton_2.setStyleSheet("background-color: rgb(200, 200, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.pushButton_3 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_3.setGeometry(QtCore.QRect(20, 260, 191, 51))
+        self.pushButton_3.setStyleSheet("background-color: rgb(200, 200, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.pushButton_4 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_4.setGeometry(QtCore.QRect(20, 320, 191, 51))
+        self.pushButton_4.setStyleSheet("background-color: rgb(170, 0, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.pushButton_5 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_5.setGeometry(QtCore.QRect(220, 140, 191, 51))
+        self.pushButton_5.setStyleSheet("background-color: rgb(0, 85, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.pushButton_6 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_6.setGeometry(QtCore.QRect(220, 200, 191, 51))
+        self.pushButton_6.setStyleSheet("background-color: rgb(0, 85, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_6.setObjectName("pushButton_6")
+        self.pushButton_7 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_7.setGeometry(QtCore.QRect(220, 260, 191, 51))
+        self.pushButton_7.setStyleSheet("background-color: rgb(0, 85, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.pushButton_8 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_8.setGeometry(QtCore.QRect(420, 140, 191, 51))
+        self.pushButton_8.setStyleSheet("background-color: rgb(0, 85, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_8.setObjectName("pushButton_8")
+        self.pushButton_9 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_9.setGeometry(QtCore.QRect(420, 200, 191, 51))
+        self.pushButton_9.setStyleSheet("background-color: rgb(0, 85, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_9.setObjectName("pushButton_9")
+        self.pushButton_10 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_10.setGeometry(QtCore.QRect(420, 320, 191, 51))
+        self.pushButton_10.setStyleSheet("background-color: rgb(200, 200, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_10.setObjectName("pushButton_10")
+        self.pushButton_11 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_11.setGeometry(QtCore.QRect(420, 260, 191, 51))
+        self.pushButton_11.setStyleSheet("background-color: rgb(200, 200, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_11.setObjectName("pushButton_11")
+        self.pushButton_12 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_12.setGeometry(QtCore.QRect(420, 380, 191, 51))
+        self.pushButton_12.setStyleSheet("background-color: rgb(170, 0, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";\n"
+"color: rgb(255, 255, 255);")
+        self.pushButton_12.setObjectName("pushButton_12")
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(20, 110, 191, 21))
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI Black")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(10)
+        self.label_3.setFont(font)
+        self.label_3.setStyleSheet("color: rgb(255, 255, 255);\n"
+"font: 87 10pt \"Segoe UI Black\";")
+        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_3.setObjectName("label_3")
+        self.label_4 = QtWidgets.QLabel(Dialog)
+        self.label_4.setGeometry(QtCore.QRect(220, 110, 191, 21))
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI Black")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(10)
+        self.label_4.setFont(font)
+        self.label_4.setStyleSheet("color: rgb(255, 255, 255);\n"
+"font: 87 10pt \"Segoe UI Black\";")
+        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_4.setObjectName("label_4")
+        self.label_5 = QtWidgets.QLabel(Dialog)
+        self.label_5.setGeometry(QtCore.QRect(420, 110, 191, 21))
+        font = QtGui.QFont()
+        font.setFamily("Segoe UI Black")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(10)
+        self.label_5.setFont(font)
+        self.label_5.setStyleSheet("color: rgb(255, 255, 255);\n"
+"font: 87 10pt \"Segoe UI Black\";")
+        self.label_5.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_5.setObjectName("label_5")
+        self.label_6 = QtWidgets.QLabel(Dialog)
+        self.label_6.setGeometry(QtCore.QRect(20, 70, 591, 21))
+        font = QtGui.QFont()
+        font.setFamily("MV Boli")
+        font.setPointSize(14)
+        self.label_6.setFont(font)
+        self.label_6.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label_6.setObjectName("label_6")
+        self.label_7 = QtWidgets.QLabel(Dialog)
+        self.label_7.setGeometry(QtCore.QRect(20, 440, 591, 21))
+        font = QtGui.QFont()
+        font.setFamily("MV Boli")
+        font.setPointSize(14)
+        self.label_7.setFont(font)
+        self.label_7.setStyleSheet("color: rgb(255, 255, 255);")
+        self.label_7.setObjectName("label_7")
+        self.progressBar = QtWidgets.QProgressBar(Dialog)
+        self.progressBar.setGeometry(QtCore.QRect(250, 470, 351, 16))
+        self.progressBar.setProperty("value", 50)
+        self.progressBar.setTextVisible(False)
+        self.progressBar.setInvertedAppearance(False)
+        self.progressBar.setObjectName("progressBar")
+        self.label_8 = QtWidgets.QLabel(Dialog)
+        self.label_8.setGeometry(QtCore.QRect(20, 470, 231, 16))
+        font = QtGui.QFont()
+        font.setFamily("MS Shell Dlg 2")
+        font.setPointSize(10)
+        font.setBold(False)
+        font.setItalic(False)
+        font.setWeight(9)
+        self.label_8.setFont(font)
+        self.label_8.setStyleSheet("color: rgb(255, 255, 255);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";")
+        self.label_8.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_8.setObjectName("label_8")
+        self.pushButton_13 = QtWidgets.QPushButton(Dialog)
+        self.pushButton_13.setGeometry(QtCore.QRect(20, 400, 71, 31))
+        self.pushButton_13.setStyleSheet("background-color: rgb(255, 255, 255);\n"
+"color: rgb(0, 0, 0);\n"
+"font: 75 10pt \"MS Shell Dlg 2\";")
+        self.pushButton_13.setObjectName("pushButton_13")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.pushButton_1.setText(_translate("Dialog", "Sortowanie bąbelkowe"))
+        self.label.setText(_translate("Dialog", "AlgoZUT"))
+        self.label_2.setText(_translate("Dialog", "TERAZ ALGORYTMY TO PESTKA !"))
+        self.pushButton_2.setText(_translate("Dialog", "Sortowanie przez wstawianie"))
+        self.pushButton_3.setText(_translate("Dialog", "Sortowanie przez zliczanie"))
+        self.pushButton_4.setText(_translate("Dialog", "Sortowanie przez scalanie"))
+        self.pushButton_5.setText(_translate("Dialog", "Wyszukiwanie minimum"))
+        self.pushButton_6.setText(_translate("Dialog", "Wyszukiwanie maksimum"))
+        self.pushButton_7.setText(_translate("Dialog", "Wyszukiwanie binarne"))
+        self.pushButton_8.setText(_translate("Dialog", "Stos"))
+        self.pushButton_9.setText(_translate("Dialog", "Kolejka"))
+        self.pushButton_10.setText(_translate("Dialog", "Lista cykliczna"))
+        self.pushButton_11.setText(_translate("Dialog", "Lista jednokierunkowa"))
+        self.pushButton_12.setText(_translate("Dialog", "Lista dwukierunkowa"))
+        self.label_3.setText(_translate("Dialog", "SORTOWANIE"))
+        self.label_4.setText(_translate("Dialog", "WYSZUKIWANIE"))
+        self.label_5.setText(_translate("Dialog", "STRUKTURY DANYCH"))
+        self.label_6.setText(_translate("Dialog", "___________________________________________________________________"))
+        self.label_7.setText(_translate("Dialog", "___________________________________________________________________"))
+        self.label_8.setText(_translate("Dialog", "Twoje postępy w nauce algorytmów:"))
+        self.pushButton_13.setText(_translate("Dialog", "Autorzy"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Dialog = QtWidgets.QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(Dialog)
+    Dialog.show()
+    sys.exit(app.exec_())
+
