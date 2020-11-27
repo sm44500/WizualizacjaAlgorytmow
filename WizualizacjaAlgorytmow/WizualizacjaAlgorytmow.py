1fb58d360fd89a0be6ba2c677c2c5013bbd37c9b
jm44340 <jm44340@zut.edu.pl>
2020-11-27 16:57:06 +0100

    Ustawiono styl domyślny na fusion

--- WizualizacjaAlgorytmow/WizualizacjaAlgorytmow.py
+++ WizualizacjaAlgorytmow/WizualizacjaAlgorytmow.py
@@ -7,6 +7,7 @@ from MainApplication import MainApplication
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setStyle("fusion")
     main_application = MainApplication()
     main_application.show()
     sys.exit(app.exec_())
