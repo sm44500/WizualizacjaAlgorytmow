1d023335880ea40ab43d704d83bed4d41da426d2
Lukasz Wieckowski <lukasz_wieckowski@zut.edu.pl>
2020-11-28 21:06:52 +0100

    modify hierarchy of algorithms

---
+++ WizualizacjaAlgorytmow/algorithms/BasicAlgorithm.py
@@ -0,0 +1,25 @@
+from Algorithm import Algorithm
+
+class BasicAlgorithm(Algorithm):
+    """
+    Klasa abstrakcyjna reprezentująca łatwy algorytm (algorytm wykorzystujący bibliotekę NetworkX)
+
+    Parametry:
+    name - skrótowa nazwa algorytmu. Tożsama z nazwą w folderze algorithm.
+
+    Przykład:
+    >>> bs = BasicAlgorithm("bubble_sort")
+    """
+    def __init__(self, name: str, title: str):
+        super().__init__(name, title)
+        
+    def add_element(self,value):
+        self.data.append(value)
+        self.save_snapshot("Dodanie wartosci " + str(value))
+        
+    def delete_element(self,value):
+        self.data.remove(value)
+        self.save_snapshot("Usuniecie wartosci " + str(value))
+        
+    def execute(self):
+        pass
