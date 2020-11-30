c90b5faa81e8c11c57efe7e962ef5c8257933696
jm44340 <jm44340@zut.edu.pl>
2020-11-30 01:11:18 +0100

    Fix join path on linux

--- WizualizacjaAlgorytmow/AlgorithmLoader.py
+++ WizualizacjaAlgorytmow/AlgorithmLoader.py
@@ -2,15 +2,11 @@ import importlib.util
 import os
 
 from Algorithms.Algorithm import Algorithm
+from Paths import Paths
 
-
-def get_algorithm_list(directory: str = "Algorithms", filename: str = "load.py") -> list:
+def get_algorithm_list() -> list:
 	"""
 	Funkcja wczytujacą wszystkie algorytmy.
-
-	Parametry:
-	directory - folder z wszystkimi algorytmami.
-	filename - nazwa pliku który zostanie wczytany z folderu.
 
 	Typ zwracany:
 	list - lista przechowująca wszystkie obiekty, które dziedziczą po Algorithm.
@@ -18,11 +14,9 @@ def get_algorithm_list(directory: str = "Algorithms", filename: str = "load.py")
 	Przykład:
 	>>> algorithms = get_algorithm_list()
 	"""
-	current_path = os.path.dirname(__file__)
-	algorithms_path = current_path + "/" + directory + "/"
-	algorithms = list()
-	for module in os.listdir(algorithms_path):
-		algorithm_directory = algorithms_path + module
+	algorithms = []
+	for module in os.listdir(Paths.algorithms):
+		algorithm_directory = Paths.algorithm(module)
 
 		if module == "__pycache__":
 			continue
@@ -30,9 +24,9 @@ def get_algorithm_list(directory: str = "Algorithms", filename: str = "load.py")
 		if not os.path.isdir(algorithm_directory):
 			continue
 
-		algorithm_init_file = algorithm_directory + "/" + filename
+		algorithm_init_file = Paths.logic(module)
 		if not os.path.isfile(algorithm_init_file):
-			print("Missing %s file! (%s)" % (filename, module))
+			print("Missing load.py file! (%s)" % (module))
 			continue
 
 		spec = importlib.util.spec_from_file_location("module.name", algorithm_init_file)
