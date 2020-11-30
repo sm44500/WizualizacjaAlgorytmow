a3d3a1a54eef7c0bd9ea0826f0f8afbeaf365635
Lukasz Wieckowski <wl44545@zut.edu.pl>
2020-11-30 22:05:08 +0100

    additional button (pause) + final version of snapshots player

--- WizualizacjaAlgorytmow/VisualisationManager.py
+++ WizualizacjaAlgorytmow/VisualisationManager.py
@@ -1,3 +1,4 @@
+from PyQt5 import QtGui
 from PyQt5.QtCore import *
 from PyQt5.QtTest import QTest
 from PyQt5.QtWidgets import *
@@ -6,104 +7,123 @@ from PyQt5.QtGui import *
 from Paths import Paths
 from Algorithms.Algorithm import Algorithm
 
+
 class VisualisationManager:
-	"""
-	Kontroler odpowiadający za wizualizację algorytmów.
+    """
+    Kontroler odpowiadający za wizualizację algorytmów.
 
-	Parametry:
-	main_widget - referencja do głównego widget'u.
-	algorithm - obiekt algorytmu.
-	"""
-	def __init__(self, main_widget: QWidget, algorithm: Algorithm):
-		self.algorithm = algorithm
-		self.main_widget = main_widget
-		self.control_panel_bottom = self.main_widget.middle_widget.right_widget.bottom_control_panel
-		self.center = self.main_widget.middle_widget.left_widget
-		self.description_widget = self.main_widget.bottom_widget
-		self.internal_widgets = []
-		self.setup_control_panel()
+    Parametry:
+    main_widget - referencja do głównego widget'u.
+    algorithm - obiekt algorytmu.
+    """
 
-	def setup_control_panel(self):
-		self.center.set_widget(self.algorithm.visualization_widget)
+    def __init__(self, main_widget: QWidget, algorithm: Algorithm):
+        self.algorithm = algorithm
+        self.main_widget = main_widget
+        self.control_panel_bottom = self.main_widget.middle_widget.right_widget.bottom_control_panel
+        self.center = self.main_widget.middle_widget.left_widget
+        self.description_widget = self.main_widget.bottom_widget
+        self.internal_widgets = []
+        self.setup_control_panel()
 
-		self.text_box_label = self.control_panel_bottom.add_label("Wartość elementu:")
-		self.text_box = self.control_panel_bottom.add_text_box()
+        self.is_playing = False
 
-		for name, on_clicked, icon, should_update_current_snapshot in self.algorithm.buttons:
-			algorithm_button = self.control_panel_bottom.add_button(name, icon)
-			algorithm_button.clicked.connect(self.on_click_algorithm)
-			algorithm_button.clicked.connect(on_clicked)
-			if should_update_current_snapshot:
-				algorithm_button.clicked.connect(self.on_click_last_snapshot)
-			self.internal_widgets.append(algorithm_button)
+    def setup_control_panel(self):
+        self.center.set_widget(self.algorithm.visualization_widget)
 
-		self.icon_panel = self.control_panel_bottom.add_icon_panel()
+        self.text_box_label = self.control_panel_bottom.add_label("Wartość elementu:")
+        self.text_box = self.control_panel_bottom.add_text_box()
 
-		self.first_step_button = self.icon_panel.add_button(Paths.icon("first.png"))
-		self.first_step_button.clicked.connect(self.on_click_first_step)
+        for name, on_clicked, icon, should_update_current_snapshot in self.algorithm.buttons:
+            algorithm_button = self.control_panel_bottom.add_button(name, icon)
+            algorithm_button.clicked.connect(self.on_click_algorithm)
+            algorithm_button.clicked.connect(on_clicked)
+            if should_update_current_snapshot:
+                algorithm_button.clicked.connect(self.on_click_last_snapshot)
+            self.internal_widgets.append(algorithm_button)
 
-		self.previous_step_button = self.icon_panel.add_button(Paths.icon("backward.png"))
-		self.previous_step_button.clicked.connect(self.on_click_previous_step)
+        self.icon_panel = self.control_panel_bottom.add_icon_panel()
 
-		self.execute_button = self.icon_panel.add_button(Paths.icon("play.png"))
-		self.execute_button.clicked.connect(self.on_click_play)
+        self.first_step_button = self.icon_panel.add_button(Paths.icon("first.png"))
+        self.first_step_button.clicked.connect(self.on_click_first_step)
 
-		self.next_step_button = self.icon_panel.add_button(Paths.icon("forward.png"))
-		self.next_step_button.clicked.connect(self.on_click_next_step)
+        self.previous_step_button = self.icon_panel.add_button(Paths.icon("backward.png"))
+        self.previous_step_button.clicked.connect(self.on_click_previous_step)
 
-		self.last_snapshot_button = self.icon_panel.add_button(Paths.icon("last.png"))
-		self.last_snapshot_button.clicked.connect(self.on_click_last_snapshot)
+        self.play_button = self.icon_panel.add_button(Paths.icon("play.png"))
+        self.play_button.clicked.connect(self.on_click_play)
 
-	def on_click_first_step(self):
-		"""
-		Zdarzenie naciśnięcia przycisku.
-		Uruchomienie pierwszego kroku, jeżeli istnieje.
-		"""
-		snapshot = self.algorithm.first_snapshot()
-		self.description_widget.set_text(snapshot.description)
-		self.center.widget.render_snapshot(snapshot)
+        self.pause_button = self.icon_panel.add_button(Paths.icon("pause.png"))
+        self.pause_button.clicked.connect(self.on_click_pause)
 
-	def on_click_previous_step(self):
-		"""
-		Zdarzenie naciśnięcia przycisku.
-		Uruchomienie poprzedniego kroku, jeżeli istnieje.
-		"""
-		snapshot = self.algorithm.previous_snapshot()
-		self.description_widget.set_text(snapshot.description)
-		self.center.widget.render_snapshot(snapshot)
+        self.next_step_button = self.icon_panel.add_button(Paths.icon("forward.png"))
+        self.next_step_button.clicked.connect(self.on_click_next_step)
 
-	def on_click_next_step(self):
-		"""
-		Zdarzenie naciśnięcia przycisku.
-		Uruchomienie następnego kroku, jeżeli istnieje.
-		"""
-		snapshot = self.algorithm.next_snapshot()
-		self.description_widget.set_text(snapshot.description)
-		self.center.widget.render_snapshot(snapshot)
+        self.last_snapshot_button = self.icon_panel.add_button(Paths.icon("last.png"))
+        self.last_snapshot_button.clicked.connect(self.on_click_last_snapshot)
 
-	def on_click_last_snapshot(self):
-		"""
-		Zdarzenie naciśnięcia przycisku.
-		Uruchomienie ostatniego kroku, jeżeli istnieje.
-		"""
-		snapshot = self.algorithm.last_snapshot()
-		self.description_widget.set_text(snapshot.description)
-		self.center.widget.render_snapshot(snapshot)
+    def on_click_first_step(self):
+        """
+        Zdarzenie naciśnięcia przycisku.
+        Uruchomienie pierwszego kroku, jeżeli istnieje.
+        """
+        self.is_playing = False
+        snapshot = self.algorithm.first_snapshot()
+        self.description_widget.set_text(snapshot.description)
+        self.center.widget.render_snapshot(snapshot)
 
-	def on_click_play(self):
-		"""
-		Zdarzenie naciśnięcia przycisku.
-		Automatyczne odtwarza algorytm krok po kroku.
-		"""
-		for i in range(len(self.algorithm.snapshots)):
-			self.on_click_next_step()
-			QTest.qWait(1000)
+    def on_click_previous_step(self):
+        """
+        Zdarzenie naciśnięcia przycisku.
+        Uruchomienie poprzedniego kroku, jeżeli istnieje.
+        """
+        self.is_playing = False
+        snapshot = self.algorithm.previous_snapshot()
+        self.description_widget.set_text(snapshot.description)
+        self.center.widget.render_snapshot(snapshot)
 
-	def on_click_algorithm(self):
-		"""
-		Obsługuje zdarzenia wewnącz algorytmu.
-		"""
-		self.algorithm.last_value = self.text_box.text()
-		#self.description_widget.set_text(snapshot.description)
-		self.text_box.clear()
+    def on_click_next_step(self):
+        """
+        Zdarzenie naciśnięcia przycisku.
+        Uruchomienie następnego kroku, jeżeli istnieje.
+        """
+        self.is_playing = False
+        snapshot = self.algorithm.next_snapshot()
+        self.description_widget.set_text(snapshot.description)
+        self.center.widget.render_snapshot(snapshot)
 
+    def on_click_last_snapshot(self):
+        """
+        Zdarzenie naciśnięcia przycisku.
+        Uruchomienie ostatniego kroku, jeżeli istnieje.
+        """
+        self.is_playing = False
+        snapshot = self.algorithm.last_snapshot()
+        self.description_widget.set_text(snapshot.description)
+        self.center.widget.render_snapshot(snapshot)
+
+    def on_click_play(self):
+        """
+        Zdarzenie naciśnięcia przycisku.
+        Automatycznie odtwarza algorytm krok po kroku.
+        """
+        self.is_playing = True
+        for i in range(len(self.algorithm.snapshots) - self.algorithm.current_snapshot_index - 1):
+            if self.is_playing:
+                self.on_click_next_step()
+                QTest.qWait(1000)
+
+    def on_click_pause(self):
+        """
+        Zdarzenie naciśnięcia przycisku.
+        Zatrzymuje automatyczne odtwarzanie algorytmu krok po kroku.
+        """
+        self.is_playing = False
+
+    def on_click_algorithm(self):
+        """
+        Obsługuje zdarzenia wewnącz algorytmu.
+        """
+        self.algorithm.last_value = self.text_box.text()
+        # self.description_widget.set_text(snapshot.description)
+        self.text_box.clear()
