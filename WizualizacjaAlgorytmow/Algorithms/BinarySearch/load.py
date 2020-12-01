d90604b268f97a16c29c07180958ff7c06d5c467
Lukasz Wieckowski <wl44545@zut.edu.pl>
2020-12-02 00:17:03 +0100

    BinarySearch - not completed

--- WizualizacjaAlgorytmow/Algorithms/BinarySearch/load.py
+++ WizualizacjaAlgorytmow/Algorithms/BinarySearch/load.py
@@ -1,5 +1,4 @@
 from Algorithms.BasicAlgorithm import BasicAlgorithm
-from Comparators import Comparator, compare
 from Snapshot import Snapshot
 
 
@@ -21,12 +20,35 @@ class BinarySearch(BasicAlgorithm):
 		"""
 		self.data.sort()
 		self.save_snapshot("Na początek sortujemy tablicę wejściową.")
+		value = self.last_value
 		left_index = 0
-		right_index = len(self.data)-1
+		right_index = len(self.data) - 1
+		found_index = -1
 
 		while left_index <= right_index:
-			center_index = int((right_index-left_index)/2)
+			center_index = (right_index + left_index) // 2
 			self.save_snapshot("Wyznaczamy element środkowy.", {center_index: Snapshot.color_selected})
+
+			if value == self.data[center_index]:
+				found_index = center_index
+				break
+			elif value < self.data[center_index]:
+				right_index = center_index - 1
+				colors = dict()
+				for i in range(left_index, right_index + 1):
+					colors[i] = Snapshot.color_selected
+				self.save_snapshot("lewa połówka.", colors)
+			else:
+				left_index = center_index + 1
+				colors = dict()
+				for i in range(left_index, right_index + 1):
+					colors[i] = Snapshot.color_selected
+				self.save_snapshot("prawa połówka.", colors)
+
+		if found_index >= 0:
+			self.save_snapshot("Znaleziono wyszukiwaną wartość w elemencie środkowym.")
+		else:
+			self.save_snapshot("nie Znaleziono wyszukiwaną wartość w elemencie środkowym.")
 
 
 def __init__():
