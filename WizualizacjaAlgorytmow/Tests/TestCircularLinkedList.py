8708ecadbd7e6f5a60f8c5a4cfa3a3dc9c4795c4
Lukasz Wieckowski <wl44545@zut.edu.pl>
2020-12-08 20:11:11 +0100

    TestCircularLinkedList - not completed

---
+++ WizualizacjaAlgorytmow/Tests/TestCircularLinkedList.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from Algorithms.CircularLinkedList.load import CircularLinkedList
+
+
+class TestCircularLinkedList(TestCase):
+	def setUp(self):
+		self.circular_linked_list = CircularLinkedList()
+
+
+class TestInit(TestCircularLinkedList):
+	def test_name(self):
+		self.assertEqual(self.circular_linked_list.name, "CircularLinkedList")
+
+	def test_title(self):
+		self.assertEqual(self.circular_linked_list.title, "Lista cykliczna")
+
+	def test_difficulty(self):
+		self.assertEqual(self.circular_linked_list.difficulty, 3)
+
