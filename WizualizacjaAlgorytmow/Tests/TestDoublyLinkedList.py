4ac00d765da75fd5e78a59c7699dc7d64d7eebf6
Lukasz Wieckowski <wl44545@zut.edu.pl>
2020-12-08 20:13:16 +0100

    TestDoublyLinkedList - not completed

---
+++ WizualizacjaAlgorytmow/Tests/TestDoublyLinkedList.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from Algorithms.DoublyLinkedList.load import DoublyLinkedList
+
+
+class TestDoublyLinkedList(TestCase):
+	def setUp(self):
+		self.doubly_linked_list = DoublyLinkedList()
+
+
+class TestInit(TestDoublyLinkedList):
+	def test_name(self):
+		self.assertEqual(self.doubly_linked_list.name, "DoublyLinkedList")
+
+	def test_title(self):
+		self.assertEqual(self.doubly_linked_list.title, "Lista dwukierunkowa")
+
+	def test_difficulty(self):
+		self.assertEqual(self.doubly_linked_list.difficulty, 3)
+
