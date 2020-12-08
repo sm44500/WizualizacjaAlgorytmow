160a199c0a8bd694ff535e94f7b1747830f02480
Lukasz Wieckowski <wl44545@zut.edu.pl>
2020-12-08 20:15:07 +0100

    TestSinglyLinkedList - not completed

---
+++ WizualizacjaAlgorytmow/Tests/TestSinglyLinkedList.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+from Algorithms.SinglyLinkedList.load import SinglyLinkedList
+
+
+class TestSinglyLinkedList(TestCase):
+	def setUp(self):
+		self.singly_linked_list = SinglyLinkedList()
+
+
+class TestInit(TestSinglyLinkedList):
+	def test_name(self):
+		self.assertEqual(self.singly_linked_list.name, "SinglyLinkedList")
+
+	def test_title(self):
+		self.assertEqual(self.singly_linked_list.title, "Lista jednokierunkowa")
+
+	def test_difficulty(self):
+		self.assertEqual(self.singly_linked_list.difficulty, 2)
+
