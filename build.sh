34624b50c3afd4536b58f06bf822d19381cfab06
jm44340 <jm44340@zut.edu.pl>
2020-12-22 18:33:58 +0100

    Build as one file

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rv ./dist/*
-pyinstaller --clean -p ./WizualizacjaAlgorytmow/ -D  WizualizacjaAlgorytmow/WizualizacjaAlgorytmow.py
+pyinstaller --clean -p ./WizualizacjaAlgorytmow/ -F WizualizacjaAlgorytmow/WizualizacjaAlgorytmow.py
 cp -rv ./WizualizacjaAlgorytmow/Algorithms ./dist/
 cp -rv ./WizualizacjaAlgorytmow/Resources ./dist/
